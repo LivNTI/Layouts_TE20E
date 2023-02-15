a10b19389135e6d547036f6d8528b2abd9e80d49
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-02-15 14:46:33 +0100

    OOPS

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Canvas {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Canvas extends JPanel implements MouseListener {
+    Color c1= Color.red;
+    Color c2= Color.magenta;
+    boolean color1= false;
+
+    public Canvas(){
+        this.setBackground(Color.green);
+        addMouseListener(this);
+        this.setSize(new Dimension(300,300));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        repaint();
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("CLICK!");
+        System.out.println(e.getButton());
+        if (e.getButton() == 1) {
+
+
+            if (color1) {
+                this.setBackground(c1);
+            } else {
+                this.setBackground(c2);
+            }
+            color1 = !color1;
+        }else {
+            this.setBackground(Color.blue);
+        }
+        repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    public Window(){
+
+        this.setTitle("Fun with layouts");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setLayout(new GridLayout(2,4));
+        for(int i=0; i<2*4;i++){
+            this.add(new Canvas());
+        }
+
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+
 }
